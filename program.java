c0c9e5c8b1fe65fd55d6afb7138cf65aa6165b10
jbliss02 <jbliss02@dcs.bbk.ac.uk>
2014-10-04 16:24:13 +0100

    program
    second commit

--- program.java
+++ program.java
@@ -1,3 +1,39 @@
-public class Program{
+public class program {
 
+	public static void main(String[] args)
+	{
+		System.out.println("test");
+	}
+
+	public static int[] GiveArrayA()
+	{
+		int[] ret = new int[8];
+		ret[0] = 1;
+		ret[1] = 4;
+		ret[2] = 2;
+		ret[3] = 5;
+		ret[4] = 7;
+		ret[5] = 4;
+		ret[6] = 8;
+		ret[7] = 6;
+		
+		return ret;
+	}
+	
+		public static int[] GiveArrayB()
+	{
+		int[] ret = new int[8];
+		ret[0] = 5;
+		ret[1] = 7;
+		ret[2] = 3;
+		ret[3] = 11;
+		ret[4] = 4;
+		ret[5] = 7;
+		ret[6] = 3;
+		ret[7] = 9;
+		
+		return ret;
+	}
+	
+	
 }
